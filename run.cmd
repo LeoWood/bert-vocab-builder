7a6eb580e2d0cb565784f28e1512cdfc600b9306
LeoWood <463861936@qq.com>
2020-06-02 20:42:37 +0800

    .

--- run.cmd
+++ run.cmd
@@ -2,4 +2,4 @@ python subword_builder.py ^
 --output_filename cscd_vovab.txt ^
 --corpus_filepattern E:\LiuHuan\Projects\UER-py\corpora\cscd_seg.txt ^
 --min_count 200 ^
---corpus_max_lines 10000000
+--corpus_max_lines 20000000
